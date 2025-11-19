f59fde8326732a0adcc593d23054e604cfa81d9d
MishaOvcharenko <misha00640064@gmail.com>
2025-11-19 05:26:50 +0300

    Создана начальная структура проекта с многоуровневой архитектурой

---
+++ src/PolyclinicSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PolyclinicSystem.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
