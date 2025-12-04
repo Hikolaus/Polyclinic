0457900e1892a40d86b7e520675515f23e8e1b40
MishaOvcharenko <misha00640064@gmail.com>
2025-12-04 11:29:14 +0300

    Внедряю функционал листа ожидания для занятых дней

---
+++ ClinicApp/Models/Core/WaitlistRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using ClinicApp.Models.DoctorModels;
+using ClinicApp.Models.PatientModels;
+
+namespace ClinicApp.Models.Core
+{
+    public class WaitlistRequest
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool IsNotified { get; set; } = false;
+
+        [ForeignKey("PatientId")]
+        public virtual Patient? Patient { get; set; }
+        [ForeignKey("DoctorId")]
+        public virtual Doctor? Doctor { get; set; }
+    }
+}
