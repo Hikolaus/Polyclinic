f3de5a454a0a1a45c496418685f28657e44dda36
MishaOvcharenko <misha00640064@gmail.com>
2025-12-07 13:31:09 +0300

    Реализую справочники лекарств и стилизацию админ-панели

---
+++ ClinicApp/wwwroot/js/reports.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    console.log('Отчеты: страница готова');
+
+    var printBtn = document.getElementById('printReport');
+    if (printBtn) {
+        printBtn.addEventListener('click', function () {
+            window.print();
+        });
+    }
+
+    initCharts();
+});
+
+function initCharts() {
+    console.log('Графики инициализированы');
+}
