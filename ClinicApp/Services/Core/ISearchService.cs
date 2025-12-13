1da7d936bd9fa645ab523382243298772f26ee32
GayOrgy31 <200249867+GayOrgy31@users.noreply.github.com>
2025-12-13 11:37:58 +0300

    Реализую поиск пациентов и просмотр их карточек

---
+++ ClinicApp/Services/Core/ISearchService.cs
@@ -0,0 +1,11 @@
+﻿using ClinicApp.Models.DoctorModels;
+using ClinicApp.Models.PatientModels;
+
+namespace ClinicApp.Services.Core
+{
+    public interface ISearchService
+    {
+        Task<List<Patient>> SearchPatientsAsync(string searchTerm);
+        Task<List<Doctor>> SearchDoctorsAsync(string searchTerm);
+    }
+}
